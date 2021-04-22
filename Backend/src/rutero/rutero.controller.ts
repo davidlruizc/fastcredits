9e055dfe12a222cad307ef7d0b5d9bae660cb4e6
Nicolás Angarita Ortiz <nicolas.angarita@masivian.com>
2021-04-22 16:17:51 -0500

    actualizacion importante

---
+++ Backend/src/rutero/rutero.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { RuteroService } from './rutero.service';
+import { CreateRuteroDto } from './dto/create-rutero.dto';
+import { UpdateRuteroDto } from './dto/update-rutero.dto';
+
+@Controller('rutero')
+export class RuteroController {
+  constructor(private readonly ruteroService: RuteroService) {}
+
+  @Post('/signUp')
+  create(@Body() createRuteroDto: CreateRuteroDto) {
+    return this.ruteroService.create(createRuteroDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.ruteroService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ruteroService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateRuteroDto: UpdateRuteroDto) {
+    return this.ruteroService.update(+id, updateRuteroDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.ruteroService.remove(+id);
+  }
+}
