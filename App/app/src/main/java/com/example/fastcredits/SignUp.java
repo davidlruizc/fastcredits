4b967c03952918ef8fa463360fff116c5b3eca4f
David Ruiz <davidl-ruizc@outlook.com>
2021-04-22 15:06:14 -0500

    fix radio button

--- App/app/src/main/java/com/example/fastcredits/SignUp.java
+++ App/app/src/main/java/com/example/fastcredits/SignUp.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUp extends Fragment {
 
@@ -14,11 +16,31 @@ public class SignUp extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View localView = inflater.inflate(R.layout.activity_sign_up, container, false);
+
+        RadioGroup radioGroup = (RadioGroup) localView .findViewById(R.id.radioGroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // checkedId is the RadioButton selected
+
+                switch(checkedId) {
+                    case R.id.et_male:
+                        // switch to fragment 1
+                        break;
+                    case R.id.et_female:
+                        // Fragment 2
+                        break;
+                }
+            }
+        });
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_sign_up, container, false);
+        return localView;
     }
 }
