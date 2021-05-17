b475ecf693750cdf03e94aced24e72304bd7b5b9
David Ruiz <davidl-ruizc@outlook.com>
2021-05-16 22:12:39 -0500

    hide header bar

--- App/app/src/main/java/com/example/fastcredits/MainActivity.java
+++ App/app/src/main/java/com/example/fastcredits/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // hide header bar
+        ((AppCompatActivity) this).getSupportActionBar().hide();
+        
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager = findViewById(R.id.viewPager);
