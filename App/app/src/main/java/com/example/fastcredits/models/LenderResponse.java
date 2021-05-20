182a54714a4fa90ea400b9b6ae0e9d2877fb5381
David Ruiz <davidl-ruizc@outlook.com>
2021-05-20 00:32:25 -0500

    serialize class response http

--- App/app/src/main/java/com/example/fastcredits/models/LenderResponse.java
+++ App/app/src/main/java/com/example/fastcredits/models/LenderResponse.java
@@ -1,9 +1,19 @@
 package com.example.fastcredits.models;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class LenderResponse {
+    @SerializedName("statusCode")
+    @Expose
+    private int statusCode;
+
+    @SerializedName("message")
+    @Expose
     private String message;
 
-    public String getMessage() {
+    @Override
+    public String toString() {
         return message;
     }
 }
