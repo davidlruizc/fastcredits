560f5c973a7216a751c477afb23ae4537eb802b5
David Ruiz <davidl-ruizc@outlook.com>
2021-05-23 13:40:33 -0500

    go to the lender activity

--- App/app/src/main/java/com/example/fastcredits/SignUp.java
+++ App/app/src/main/java/com/example/fastcredits/SignUp.java
@@ -3,35 +3,18 @@ package com.example.fastcredits;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.Toast;
-
-import com.example.fastcredits.models.ApiResponse;
-import com.example.fastcredits.models.Countries;
-import com.example.fastcredits.models.Lender;
-import com.example.fastcredits.services.ApiAdapter;
-
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class SignUp extends Fragment {
     String gender;
-    Spinner spinnerCountry;
 
     public SignUp() {
         // Required empty public constructor
@@ -46,18 +29,46 @@ public class SignUp extends Fragment {
 
         radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
             switch(checkedId) {
-                case R.id.et_male:
-                    gender = "Masculino";
+                case R.id.et_clients:
+                    gender = getString(R.string.profile_clients);
                     break;
-                case R.id.et_female:
-                    gender = "Femenino";
+                case R.id.et_lender:
+                    gender = getString(R.string.profile_lender);
+                    break;
+                case R.id.et_router:
+                    gender = getString(R.string.profile_router);
+                    break;
+                case R.id.et_admin:
+                    gender = getString(R.string.profile_admin);
                     break;
             }
         });
 
         final Button button = localView.findViewById(R.id.btn_register);
         button.setOnClickListener(v -> {
-            Toast.makeText(getContext(), "BIBIBIBIIBIB", Toast.LENGTH_SHORT).show();
+            if (gender != null) {
+                // TODO: set all activities for each option
+                switch(gender) {
+                    case "Cliente":
+                        Toast.makeText(getContext(), "Cliente", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Prestamista":
+                        startActivity(new Intent(getActivity(), SignUpLenderActivity.class));
+                        break;
+                    case "Rutero":
+                        Toast.makeText(getContext(), "Rutero", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Administrador":
+                        Toast.makeText(getContext(), "Admin", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            } else {
+                new AlertDialog.Builder(getContext())
+                        .setTitle("¡Ups!")
+                        .setMessage("Para poder continuar debes seleccionar un tipo de perfil")
+                        .setPositiveButton("Aceptar", (dialog, which) -> Log.d("MainActivity", "Sending atomic bombs to Jupiter"))
+                        .show();
+            }
         });
 
         // Inflate the layout for this fragment
