113e544cc652ab1b7fd14355278c17bbad696bfe
David Ruiz <davidl-ruizc@outlook.com>
2021-06-03 16:05:01 -0500

    list all lenders

--- App/app/src/main/java/com/example/fastcredits/ui/usersAllLenders/UsersAllLenders.java
+++ App/app/src/main/java/com/example/fastcredits/ui/usersAllLenders/UsersAllLenders.java
@@ -3,64 +3,86 @@ package com.example.fastcredits.ui.usersAllLenders;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.fastcredits.R;
+import com.example.fastcredits.models.Lender;
+import com.example.fastcredits.models.Lenders;
+import com.example.fastcredits.services.ApiAdapter;
+import com.example.fastcredits.ui.dashboard.CardAdminAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UsersAllLenders#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
 public class UsersAllLenders extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UsersAllLenders() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UsersAllLenders.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UsersAllLenders newInstance(String param1, String param2) {
-        UsersAllLenders fragment = new UsersAllLenders();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private RecyclerView lenderCardRV;
+    private ArrayList<Lender> lendersArrayList;
+    private CardLendersAdapter cardLendersAdapter;
+    private ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_all_lenders, container, false);
+        View root = inflater.inflate(R.layout.fragment_users_all_lenders, container, false);
+
+        // initializing our variables.
+        lenderCardRV = root.findViewById(R.id.user_all_lenders_recycler);
+        progressBar = root.findViewById(R.id.user_all_lenders_loading);
+
+        // creating new array list.
+        lendersArrayList = new ArrayList<>();
+
+        GetAllLenders();
+
+        return root;
+    }
+
+    public void GetAllLenders() {
+        Call<Lenders> call = ApiAdapter.getApiService().getAllLenders();
+        call.enqueue(new Callback<Lenders>() {
+            @Override
+            public void onResponse(Call<Lenders> call, Response<Lenders> response) {
+                if (response.isSuccessful()) {
+                    // on successful we are hiding our progressbar.
+                    progressBar.setVisibility(View.GONE);
+
+                    // below line is to add our data from api to our array list.
+                    lendersArrayList = response.body().getLenders();
+
+                    // below line we are running a loop to add data to our adapter class.
+                    for (int i = 0; i < lendersArrayList.size(); i++) {
+                        cardLendersAdapter = new CardLendersAdapter(lendersArrayList, getContext());
+
+                        // below line is to set layout manager for our recycler view.
+                        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+
+                        // setting layout manager for our recycler view.
+                        lenderCardRV.setLayoutManager(manager);
+
+                        // below line is to set adapter to our recycler view.
+                        lenderCardRV.setAdapter(cardLendersAdapter);
+                    }
+                } else {
+                    Toast.makeText(getContext(), "not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Lenders> call, Throwable t) {
+                Toast.makeText(getContext(), "network failure :( inform the user and possibly retry", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
